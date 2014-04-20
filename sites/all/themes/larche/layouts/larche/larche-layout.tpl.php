a652ff6b69ff46df9ea3f48d80857e3fe2e5105a
Petro Kashpurenko <masterpoint@Petros-MacBook-Pro.local>
2014-04-20 22:18:50 +0100

    Fixed footer style bug

--- sites/all/themes/larche/layouts/larche/larche-layout.tpl.php
+++ sites/all/themes/larche/layouts/larche/larche-layout.tpl.php
@@ -1,39 +1,41 @@
 <div<?php print $attributes; ?>>
   <div class="l-header-wrapper">
-  <header class="l-header" role="banner">
-    <div class="l-branding">
-      <?php if ($logo): ?>
-        <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="site-logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
-      <?php endif; ?>
-
-      <?php if ($site_name || $site_slogan): ?>
-        <?php if ($site_name): ?>
-          <h1 class="site-name">
-            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-          </h1>
+    <header class="l-header" role="banner">
+      <div class="l-branding">
+        <?php if ($logo): ?>
+          <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="site-logo"><img
+              src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>"/></a>
         <?php endif; ?>
 
-        <?php if ($site_slogan): ?>
-          <h2 class="site-slogan"><?php print $site_slogan; ?></h2>
+        <?php if ($site_name || $site_slogan): ?>
+          <?php if ($site_name): ?>
+            <h1 class="site-name">
+              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"
+                 rel="home"><span><?php print $site_name; ?></span></a>
+            </h1>
+          <?php endif; ?>
+
+          <?php if ($site_slogan): ?>
+            <h2 class="site-slogan"><?php print $site_slogan; ?></h2>
+          <?php endif; ?>
         <?php endif; ?>
-      <?php endif; ?>
 
-      <?php print render($page['branding']); ?>
-    </div>
+        <?php print render($page['branding']); ?>
+      </div>
 
-    <?php print render($page['header']); ?>
-    <?php print render($page['navigation']); ?>
-  </header>
+      <?php print render($page['header']); ?>
+      <?php print render($page['navigation']); ?>
+    </header>
   </div>
 
   <div class="l-header-compensation"></div>
 
   <?php if (isset($page['slider'])): ?>
-  <div class="l-slider-wrapper">
-    <div class="l-slider">
-      <?php print render($page['slider']); ?>
+    <div class="l-slider-wrapper">
+      <div class="l-slider">
+        <?php print render($page['slider']); ?>
+      </div>
     </div>
-  </div>
   <?php endif; ?>
 
   <div class="l-main">
@@ -61,20 +63,21 @@
   </div>
 
   <?php if (isset($page['column_first']) || isset($page['column_second'])): ?>
-  <div class="l-columns">
-    <div class="l-column-first">
-      <?php print render($page['column_first']); ?>
+    <div class="l-columns">
+      <div class="l-column-first">
+        <?php print render($page['column_first']); ?>
+      </div>
+      <div class="l-column-second">
+        <?php print render($page['column_second']); ?>
+      </div>
     </div>
-    <div class="l-column-second">
-      <?php print render($page['column_second']); ?>
-    </div>
-  </div>
   <?php endif; ?>
 
   <div class="l-footer-wrapper">
     <footer class="l-footer" role="contentinfo">
-      <div class="footer-logo"/></div>
+      <div class="footer-logo"></div>
       <?php print render($page['footer']); ?>
     </footer>
   </div>
+
 </div>
